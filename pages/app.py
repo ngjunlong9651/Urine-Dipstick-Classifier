aceebd9a4c6e044673d4c6fec52567ee9c0cb8ce
ngjunlong9651 <junlong965@gmail.com>
2023-09-26 17:30:08 +0800

    trying to resize images

--- pages/app.py
+++ pages/app.py
@@ -2,6 +2,7 @@ import os
 import streamlit as st
 from PIL import Image, ImageOps
 from streamlit_option_menu import option_menu
+import numpy as np
 
 with st.sidebar:
     selected = option_menu("Urine Dipstick Scoring App", ["Home", 'Urine Dipstick Scoring App', 'Next Steps'], 
@@ -16,9 +17,17 @@ if option == "Upload an image":
     device_file = st.file_uploader("", type=["jpg", "png", "jpeg"])
     if device_file is not None:
         image = Image.open(device_file)
-        st.image(image, use_column_width=True)
+        size = (224, 224)
+        image = ImageOps.fit(image, size, Image.ANTIALIAS)
+        img = np.asarray(image)
+        img_reshape = img[np.newaxis,...]
+        st.image(img_reshape, use_column_width=True)
 else:
     device_capture = st.camera_input("Take a picture of the dipstick")
     if device_capture is not None:
         image = Image.open(device_capture)
-        st.image(image)
+        size = (224, 224)
+        image = ImageOps.fit(image, size, Image.ANTIALIAS)
+        img = np.asarray(image)
+        img_reshape = img[np.newaxis,...]
+        st.image(img_reshape, use_column_width=True)
