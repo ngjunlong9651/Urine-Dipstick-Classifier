4065ccb940c1d4dfea631cde1da418a71dc62283
ngjunlong9651 <junlong965@gmail.com>
2023-09-26 17:38:30 +0800

    revert change

--- pages/app.py
+++ pages/app.py
@@ -17,13 +17,9 @@ if option == "Upload an image":
     device_file = st.file_uploader("", type=["jpg", "png", "jpeg"])
     if device_file is not None:
         image = Image.open(device_file)
-        size = (224, 224)
-        image = ImageOps.fit(image, size, "ANTIALIAS")
         st.image(image, use_column_width=True)
 else:
     device_capture = st.camera_input("Take a picture of the dipstick")
     if device_capture is not None:
         image = Image.open(device_capture)
-        size = (224, 224)
-        image = ImageOps.fit(image, size, "ANTIALIAS")
         st.image(image, use_column_width=True)
