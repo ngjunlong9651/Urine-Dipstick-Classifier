67349e55c572f01e3904f260a5f947d8f5002997
ngjunlong9651 <junlong965@gmail.com>
2023-09-26 17:02:21 +0800

    Added button to upload images from device

--- pages/app.py
+++ pages/app.py
@@ -1,6 +1,6 @@
 import os
 import streamlit as st ## Importing stream lit
-from PIL import Image ## Importing image holder function
+from PIL import Image, ImpageOps ## Importing image holder function
 from streamlit_option_menu import option_menu ## Importing option menu, to allow toggling between pages
 from camera_input_live import camera_input_live ## Importing camera input function
 
@@ -10,9 +10,11 @@ with st.sidebar:
         icons=['house-door-fill', 'hospital-fill', 'emoji-smile-fill'], menu_icon="cast", default_index=1)
     selected
     
-st.write("This project was prepared fro Prof Teo # Placeholder text ")
+st.write("Upload an image from your device")
 # # image = Image.open('pneumonia inception v3.jpeg')
 # # st.image(image, caption='Placeholder image',use_column_width=True)
+file = st.file_uploader("", type=["jpg", "png", "jpeg"])
+
 
 image = camera_input_live()
 st.image(image)
